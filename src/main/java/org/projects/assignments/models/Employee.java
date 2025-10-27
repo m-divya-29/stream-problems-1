5098cb82a4cb026161363591952a19cb54ba038c
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2025-10-27 18:03:21 +0530

    ✨ Add EmployeeCustomView record and constructor to Employee

--- src/main/java/org/projects/assignments/models/Employee.java
+++ src/main/java/org/projects/assignments/models/Employee.java
@@ -1,4 +1,8 @@
 package org.projects.assignments.models;
 
 public record Employee(String name, String department, Double salary) {
+
+	public Employee(String string, String string2, int i) {
+		this(string, string2, Double.valueOf(i));
+	}
 }
---
+++ src/main/java/org/projects/assignments/models/EmployeeCustomView.java
@@ -0,0 +1,7 @@
+package org.projects.assignments.models;
+
+import java.util.List;
+
+public record EmployeeCustomView(List<String> empNameList, double totalSalary) {
+
+}
